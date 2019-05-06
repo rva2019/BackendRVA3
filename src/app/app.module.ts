8ec6139406c8ee8d7e766a339c4e09aa2292318a
Marko Bojkic <markobojkic@ftn.ns>
2019-05-06 08:36:58 +0200

    Vezbe 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VoziloComponent } from './components/vozilo.component';
+import { VoziloComponent } from './components/vozilo/vozilo.component';
 import { AutomobilComponent } from './components/automobil/automobil.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
