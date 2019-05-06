e70eb09ca1c372c0fdf68cc0235b94a392833688
Marko Bojkic <markobojkic@ftn.ns>
2019-05-06 08:32:53 +0200

    Vezbe 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './components/vozilo.component';
+import { AutomobilComponent } from './components/automobil/automobil.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatButtonModule, 
+          MatIconModule, 
+          MatSidenavModule, 
+          MatListModule, 
+          MatExpansionModule, 
+          MatGridListModule } from '@angular/material';
+import { AboutComponent } from './components/core/about/about.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { ArtiklComponent } from './components/artikl/artikl.component';
+import { DobavljacComponent } from './components/dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './components/porudzbina/porudzbina.component';
+import { StavkaPorduzbineComponent } from './components/stavka-porduzbine/stavka-porduzbine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    AboutComponent,
+    HomeComponent,
+    AuthorComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorduzbineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/stavka-porduzbine/stavka-porduzbine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stavka-porduzbine',
+  templateUrl: './stavka-porduzbine.component.html',
+  styleUrls: ['./stavka-porduzbine.component.css']
+})
+export class StavkaPorduzbineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
